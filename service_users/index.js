51a23ef5541f91484beab907d56eaaf29e5feb5d
TRUE_EGOISTUS <goldenfreddy6661@gmail.com>
2025-10-17 18:15:11 +0300

    Установка зависимости pino

--- service_users/index.js
+++ service_users/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const Joi = require('joi');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();

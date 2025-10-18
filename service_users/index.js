c928abc3a0c4d83c1f28b729f52aaa845cbcea4c
TRUE_EGOISTUS <goldenfreddy6661@gmail.com>
2025-10-18 13:14:13 +0300

    Убрана переменная currentId и небольшое дополнение валидации

--- service_users/index.js
+++ service_users/index.js
@@ -46,11 +46,11 @@ const authenticateJWT = (req, res, next) => {
 };
 // Имитация базы данных в памяти (LocalStorage)
 let fakeUsersDb = {};
-let currentId = 1;
 
 const registerSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
+    name: Joi.string().allow('').optional(),
     role: Joi.string().valid('user', 'admin').default('user')
 });
 const loginSchema = Joi.object({
